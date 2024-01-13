b01b1b5ca259f24107d1bddfe77c71644b0e7706
soydavido <ortuod@gmail.com>
2024-01-13 13:27:11 -0400

    Contando del 1 al 25

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ for (let index = 1; index <= 5; index++) {
     console.log(index);
 }
 
-console.log('Contar los numeros del 20 al 25');
+console.log('Contar los numeros del 6 al 25');
 
-for (let index = 20; index <= 25; index++) {
+for (let index = 6; index <= 25; index++) {
     console.log(index);
 }
